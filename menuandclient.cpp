656e3db6e52badd9e26d44fe251526df6455a273
Mahlet59 <88796858+Mahlet59@users.noreply.github.com>
2021-09-02 18:37:13 +0300

    Add files via upload

---
+++ menuandclient.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <string>
+using namespace std;
+struct seating{
+ 	int row;
+ 	
+ 	char colomn;
+ 	
+ };
+struct seatingClass {
+ 	string level;
+ };
+struct additionalBeverages{
+ 	string food;
+ 	string drink;
+ };
+ struct toWatch{
+ 	string movieName;
+ 	float score;
+ 	char rating;
+ };
+ struct info{
+    string name;
+    int age;
+    seating place;
+    seatingClass package;
+    additionalBeverages snack;
+    toWatch show;
+    }input[200];
+
+inline void arrangment(){
+cout<<"\n\n____________________________________________________";
+cout<<"\n1A 1B 1C 1D 1E 1F 1G 1H 1I 1J 1K 1L 1M 1N 1O 1P\n2A 2B 2C 2D 2E 2F 2G 2H 2I 2J 2K 2L 2M 2N 2O 2P";
+cout<<"\n3A 3B 3C 3D 3E 3F 3G 3H 3I 3J 3K 3L 3M 3N 3O 3P\n4A 4B 4C 4D 4E 4F 4G 4H 4I 4J 4K 4L 4M 4N 4O 4P";
+cout<<"\n____________________________________________________"<<endl;
+}
+inline void foodoption(){
+    
+    cout<<"\t\tSnack\t\t\tdrink"<<endl;
+    cout<<"\t\tPopcorn\t\t\tSprite(S/L)"<<endl;
+    cout<<"\t\tSunchips\t\tFanta Orange(S/L)"<<endl;
+    cout<<"\t\tMr.Potato\t\tCoca-Cola(S/L)"<<endl;
+    cout<<"\t\tNone";
+}
+int main (){
+int num,choice;
+
+  
+cout<<"\n\t\t\tWELLCOME TO MOVIE/SHOW BOOKING SYSTEM"<<endl;
+cout<<"\nPress 1 to book a ticket: \nPress 2 to view movie rating: "<<endl;
+cin>>choice;
+switch(choice){
+    case 1:
+   cout<<"\nPlease enter the number of tickets you want to book: ";
+cin>>num; 
+
+    for (int i=0;i<num;i++){
+    
+    cout<<"\nPlease enter the name of client number "<<i+1<<":";
+    cin>>(input+i)->name;
+    cout<<"\nPlease enter your age:";
+    cin>>(input+i)->age;
+    if ((input+i)->age<18){
+        cout<<"\nPlease choose a PG-rated movies";
+    }
+    else{
+        cout<<"\nR-rated movies are available as well";
+    }
+    cout<<"\nPlease enter the movie of your choice:";
+cin>>(input+i)->show.movieName;
+cout<<"\nPlease enter the package you want. Available packages are normal, gold and platnium. ";
+cin>>(input+i)->package.level;
+cout<<"\nPlease enter the seating arrangment you want:";
+arrangment();
+cout<<"\nEnter the row(1-4):";
+cin>>(input+i)->place.row;
+cout<<"\nEnter the colomn(A-P):";
+cin>>(input+i)->place.colomn;
+cout<<"\n\t\tPlease choose a snack and drink."<<endl;
+foodoption();
+cout<<"\nwhich snack would you like? ";
+cin>>(input+i)->snack.food;
+cout<<"which drink would you like? ";
+cin>>(input+i)->snack.drink;
+}
+for (int i=0;i<num;i++){
+    cout<<"\n\t\tDear customer you have successfully booked a tiket to watch "<<(input+i)->show.movieName<<".";
+    cout<<"\n\t\tPlease grab your snack along with your drinks (if you bought any) and be seated at "<<(input+i)->package.level<<" class, "<<(input+i)->place.row<<(input+i)->place.colomn<<".";
+   cout<<"\n\t\t\t\t\t\tENJOY";
+
+
+}
+break;
+
+}
+
+  return 0;
+}
